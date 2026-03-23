e175c2efe150700f38178b8dfd51779fbb3505d8
Snezana <snmitrov@cisco.com>
2026-03-23 18:21:48 +0100

    remove tf cloud

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  cloud {
-    organization = "CX-Branch"
-
-    workspaces {
-      name = "bac-lab"
-    }
-  }
-}
-
 module "meraki" {
   source           = "github.com/netascode/terraform-meraki-nac-meraki?ref=v0.6.0"
   yaml_directories = ["data"]
